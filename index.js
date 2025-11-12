6723640c12015c9cdda6a4c73a74200db6220539
Dip Dey <dipdey993@gmail.com>
2025-11-12 20:26:09 +0600

    feat: update response message for root route and add .vercel to .gitignore

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Fin Track");
 });
 
 app.get("/quote", async (req, res) => {
